ef62eb05e1ce7718e211ef8424d5fa03766ffbe2
Steven Rémot <steven.remot@gmail.com>
2015-03-07 21:35:40 +0100

    Remove tabs in main.js

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ class App {
     constructor() {
         let scene = new THREE.Scene();
         let camera = new THREE.PerspectiveCamera(
-	    75,
-	    window.innerWidth / window.innerHeight,
-	    0.1,
-	    1000
-	);
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
         let renderer = new THREE.WebGLRenderer();
 
         let renderContext = new RenderContext(renderer, camera, scene);
