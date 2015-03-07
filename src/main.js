18ee2e275a1f0a7849ae2ffeb03b7dd16c5d1de7
kazmiero <alex.kazmierowski@gmail.com>
2015-03-07 19:41:24 +0100

    Add basic camera class

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import THREE from "mrdoob/three.js";
 import {KeyboardInput} from './input/keyboard';
+import {Camera} from './render/camera';
 
 class App {
     constructor() {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.camera = new Camera(
+	    new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+	);
         this.renderer = new THREE.WebGLRenderer();
 
         this.renderer.setSize( window.innerWidth, window.innerHeight );
@@ -17,7 +20,7 @@ class App {
         let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         this.cube = new THREE.Mesh( geometry, material );
         this.scene.add(this.cube);
-        this.camera.position.z = 5;
+        this.camera.move(new THREE.Vector3(0,0,5));
     }
 
     update() {
---
+++ src/render/camera.js
@@ -0,0 +1,41 @@
+import THREE from "mrdoob/three.js";
+
+/**
+ * @description
+ * Camera
+ */
+export class Camera {
+    constructor(threeCamera) {
+	_threeCamera = threeCamera
+    };
+
+    lookAt(vector) {
+	_threeCamera.lookAt(vector)
+    };
+
+    move(vector) {
+	_threeCamera.position.x += vector.x
+	_threeCamera.position.y += vector.y
+	_threeCamera.position.z += vector.z
+    };
+
+    setPosition(position) {
+	_threeCamera.position = position
+    };
+    
+    /**
+     * @description
+     * initialize the camera behind and slightly above the ship
+     * @param shipPosition: Vector3
+     * @param shipOrientation: EulerAngles? Quaternion?
+    */
+    setup(shipPosition, shipOrientation) {
+	setPosition(shipPosition)
+
+	// todo: define/compute intelligentOffset
+	let intelligentOffset = new THREE.Vector3(0,0,0)
+	move(intelligentOffset)
+
+	// todo: manage orientation
+    };
+};
