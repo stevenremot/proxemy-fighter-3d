ea88882dbfb401376efe9a0adc16d08da4b8d064
Steven Rémot <steven.remot@gmail.com>
2015-03-07 21:35:16 +0100

    Remove tabs in camera.js

--- src/render/camera.js
+++ src/render/camera.js
@@ -6,25 +6,25 @@ import THREE from "mrdoob/three.js";
  */
 export class Camera {
     constructor(threeCamera) {
-	    this._threeCamera = threeCamera;
+        this._threeCamera = threeCamera;
     }
 
     get threeCamera() {
-	    return this._threeCamera;
+        return this._threeCamera;
     }
 
     lookAt(vector) {
-	    this._threeCamera.lookAt(vector);
+        this._threeCamera.lookAt(vector);
     }
 
     move(vector) {
-	    this._threeCamera.position.x += vector.x;
-	    this._threeCamera.position.y += vector.y;
-	    this._threeCamera.position.z += vector.z;
+        this._threeCamera.position.x += vector.x;
+        this._threeCamera.position.y += vector.y;
+        this._threeCamera.position.z += vector.z;
     }
 
     setPosition(position) {
-	    this._threeCamera.position = position;
+        this._threeCamera.position = position;
     }
 
     /**
@@ -34,12 +34,12 @@ export class Camera {
      * @param shipOrientation: EulerAngles? Quaternion?
      */
     setup(shipPosition, shipOrientation) {
-	    this.setPosition(shipPosition);
+        this.setPosition(shipPosition);
 
-	    // todo: define/compute intelligentOffset
-	    let intelligentOffset = new THREE.Vector3(0,0,0);
-	    this.move(intelligentOffset);
+        // todo: define/compute intelligentOffset
+        let intelligentOffset = new THREE.Vector3(0,0,0);
+        this.move(intelligentOffset);
 
-	    // todo: manage orientation
+        // todo: manage orientation
     }
 }
