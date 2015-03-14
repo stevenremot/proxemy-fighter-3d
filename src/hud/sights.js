5a5df265b8bec1652677aa9a806b5d740f82c0bc
Alexandre Kazmierowski <alex.kazmierowski@gmail.com>
2015-03-14 12:16:40 +0100

    Correct aim vertical offset

--- src/hud/sights.js
+++ src/hud/sights.js
@@ -28,7 +28,9 @@ export class Sights {
         this.domElement.style.top = `${this._position[1]}px`;
 
         // update camera's x_relative and y_relative
+        // todo: there seems to be a lateral offset between the screen's x position and the camera 
+        // far plane position that increase when whe deviate from the center of the screen
         camera.x_relative = (this._position[0] + this._halfWidth) / this._window.innerWidth;
-        camera.y_relative = this._position[1] / this._window.innerHeight;
+        camera.y_relative = (this._position[1] - this._halfHeight) / this._window.innerHeight;
     }
 }
