6093459c0d721ce1bb6b529028ab1190238b9dfa
Steven Rémot <steven.remot@inovia-team.com>
2015-04-03 13:23:25 +0200

    Use real names in copyright

--- src/render/camera.js
+++ src/render/camera.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 The Proxemy Fighter 3D Team
+ * Copyright (C) 2015 Alexandre Kazmierowski, Steven Rémot
  * Licensed under the General Public License, see the file gpl.txt at the root for details.
  */
 
